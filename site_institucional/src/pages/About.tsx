77fee0cd4baf01d1a96a7d1af09d131ea8cbd258
Daniel Mahl Gregorini <danielmagregorini123@gmail.com>
2024-05-21 09:39:23 -0300

    mundando a cor de fundo

--- site_institucional/src/pages/About.tsx
+++ site_institucional/src/pages/About.tsx
@@ -5,8 +5,8 @@ function About(): JSX.Element {
         <div className='w-full '>
 
             <div>
-                <h1 className='text-6xl text-center mt-10'>titul</h1>
-                <p className='text-center mt-2 text-xl'>sdadasdasdasd</p>
+                <h1 className='text-6xl text-center mt-10'>titulo pagina sobre</h1>
+                <p className='text-center mt-2 text-xl'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere ab vitae sed, aperiam earum blanditiis praesentium in cumque aliquid sit molestias, tempora iure ad nostrum dolorem rerum libero beatae laboriosam dolores modi, soluta nihil. Odit tenetur aliquam delectus excepturi velit ratione et impedit qui fugit quo quidem libero, ullam fugiat explicabo quibusdam, eligendi voluptas dolor accusamus porro. Dolore ullam nulla voluptas exercitationem officia, id sit maiores obcaecati, hic explicabo unde neque veritatis aliquid, omnis sequi eaque quidem. Quisquam sequi enim ea odio inventore distinctio, exercitationem, ipsam sed ab dolore mollitia saepe suscipit porro natus facilis accusantium delectus similique laboriosam incidunt.</p>
             </div>
             
         </div>
